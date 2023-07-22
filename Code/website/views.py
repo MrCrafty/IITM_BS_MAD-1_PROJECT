a2731160cd09625d9d4e439f10c0daedb6f5671e
Harsh <harsh.mithapara@gmail.com>
2023-07-22 10:52:50 +0530

    added about route

--- Code/website/views.py
+++ Code/website/views.py
@@ -6,3 +6,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def Home():
     return render_template('Home.html')
+
+
+@views.route('/about')
+def About():
+    return render_template('About.html')
